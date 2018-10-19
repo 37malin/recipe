baaab3441dcfb12bce29934f474fbb8e8d6afdc8
EC2 Default User <ec2-user@ip-172-31-18-212.ap-southeast-1.compute.internal>
2018-10-19 23:18:46 +0000

    Initialize repository

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def index
+  end
+  
+  def create
+    @post = Post.new(title: params[:title], zairyo: params[:zairyo], step: params[:step])
+    @post.save
+    redirect_to("/posts/index")
+  end
+end
