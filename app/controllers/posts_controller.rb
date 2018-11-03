9e547d184286516da068077b7e9fadd98633f8e1
EC2 Default User <ec2-user@ip-172-31-18-212.ap-southeast-1.compute.internal>
2018-11-03 01:16:11 +0000

    Create Edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,18 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.zairyo = params[:zairyo]
+    @post.step = params[:step]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
 end
